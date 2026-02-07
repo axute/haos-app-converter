a066676d241a82d1a4416fd3c461cbdb16108914
axute <axutes@gmail.com>
2026-02-07 17:18:51 +0100

    fix: make data directory path configurable via environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -e
 
+# Pfad aus Umgebungsvariable oder Standardpfad
+DATA_DIR="${CONVERTER_DATA_DIR:-/var/www/html/data}"
+
 # Sicherstellen, dass das data-Verzeichnis existiert
-mkdir -p /var/www/html/data
+mkdir -p "$DATA_DIR"
 
 # Berechtigungen für das data-Verzeichnis setzen, damit der Webserver (www-data) schreiben kann
-chown -R www-data:www-data /var/www/html/data
-chmod -R 775 /var/www/html/data
+chown -R www-data:www-data "$DATA_DIR"
+chmod -R 775 "$DATA_DIR"
 
 # Den ursprünglichen Befehl (Apache) ausführen
 exec apache2-foreground
